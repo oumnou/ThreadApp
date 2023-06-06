0f2cdfb5002b42976549adad3e35988f0c46b69e
MAR BEN <marouanebenbajja@gmail.com>
2023-06-06 22:16:04 +0100

    attach resolved

--- app/src/main/java/com/example/exercice_07/SignUpFragment.java
+++ app/src/main/java/com/example/exercice_07/SignUpFragment.java
@@ -7,7 +7,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -115,4 +117,12 @@ public class SignUpFragment extends Fragment {
             }
     });
 }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
 }
